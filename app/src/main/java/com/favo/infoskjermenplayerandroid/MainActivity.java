9db8f4bcfae32202768f3b81f6857426171d71ef
Georg Skuggedal <georgskuggedal@icloud.com>
2020-08-24 14:48:22 +0200

    Initial commit

--- app/src/main/java/com/favo/infoskjermenplayerandroid/MainActivity.java
+++ app/src/main/java/com/favo/infoskjermenplayerandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
@@ -11,17 +12,27 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
 import android.util.Log;
 import org.json.JSONObject;
 
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+
+/* To Do:
+    Kontroller inputs, finne ut keycodes.
+*/
 
 public class MainActivity extends Activity{
 
@@ -31,6 +42,12 @@ public class MainActivity extends Activity{
     private String host = "http://app.infoskjermen.no";
     private String TAG = "Message";
     private String physical_id;
+
+    public static SharedPreferences prefs;
+
+    private Button btn;
+    private EditText hostinput;
+    private LinearLayout modal;
 
     private int mediumAnimationDuration;
 
@@ -42,8 +59,16 @@ public class MainActivity extends Activity{
         splashscreen = findViewById(R.id.splashscreen);
         webview = findViewById(R.id.webview);
 
+
         splashscreen.getSettings().setJavaScriptEnabled(true);
         splashscreen.loadUrl("file:///android_asset/splashscreen.html");
+
+        prefs = this.getSharedPreferences("Infoskjermen", Context.MODE_PRIVATE);
+        String app_host = prefs.getString("app_host", null);
+        if (app_host != null ) {
+            host = app_host;
+        }
+        ChangeHostModal();
 
         webview.setVisibility(View.GONE);
 
@@ -68,9 +93,10 @@ public class MainActivity extends Activity{
 
 
     private void WebviewLoadGo() {
-        webview = findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
         webview.loadUrl(host + "/go");
 
         webview.setWebViewClient(new WebViewClient() {
@@ -87,6 +113,22 @@ public class MainActivity extends Activity{
             }
         });
 
+    }
+
+    private void ChangeHostModal() {
+        btn = findViewById(R.id.hostbtn);
+        hostinput = findViewById(R.id.hostinput);
+        modal = findViewById(R.id.modal);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               Editable inputhost = hostinput.getText();
+                Log.i(TAG, "onClick: ");
+                Log.i(TAG, inputhost.toString());
+                webview.loadUrl(inputhost + "/go");
+                modal.setVisibility(View.GONE);
+           }
+        });
     }
 
     private void CrossFadeWebViews() {
@@ -175,6 +217,26 @@ public class MainActivity extends Activity{
         return activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
     }
+
+
+    private String codes = "";
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        Log.i("KEY", "" + event.getKeyCode());
+
+        codes += "-" + event.getKeyCode();
+
+        if (codes.endsWith("")) {
+            webview.reload();
+        } else if (event.getAction() == KeyEvent.ACTION_UP) {
+            Log.e(TAG, "Key up, code " + event.getKeyCode());
+        }
+
+        return true;
+    }
+
+
 
     private class SendDeviceInfo extends AsyncTask<String, Void,Void> {
 
